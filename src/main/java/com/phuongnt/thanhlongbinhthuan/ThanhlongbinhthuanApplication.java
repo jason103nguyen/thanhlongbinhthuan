59e332f9a239b42884f704d35c3715bc69e83c25
Nguyen Tri Phuong <jason103nguyen@gmail.com>
2021-12-04 10:47:01 +0700

    create project

---
+++ src/main/java/com/phuongnt/thanhlongbinhthuan/ThanhlongbinhthuanApplication.java
@@ -0,0 +1,13 @@
+package com.phuongnt.thanhlongbinhthuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThanhlongbinhthuanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThanhlongbinhthuanApplication.class, args);
+	}
+
+}
